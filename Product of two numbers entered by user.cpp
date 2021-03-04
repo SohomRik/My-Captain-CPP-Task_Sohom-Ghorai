bcfe0f4c33eee2b2efb6d407d0b8c2117cc8f70b
SohomRik <80033329+SohomRik@users.noreply.github.com>
2021-03-04 14:32:45 +0530

    Add files via upload

---
+++ Product of two numbers entered by user.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,r;
+	cout<<"Enter a number : ";
+	cin>>a;
+	cout<<"Enter another number : ";
+	cin>>b;
+	r = a*b;
+	cout<<"Product of "<< a <<" & "<<b<<" is : "<<r;
+	
+	return 0;
+	
+}
