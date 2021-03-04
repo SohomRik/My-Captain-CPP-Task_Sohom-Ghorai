d3c904d4316c299186eca556a7c7f9807bb867c5
SohomRik <80033329+SohomRik@users.noreply.github.com>
2021-03-04 14:35:42 +0530

    Update Product of two numbers entered by user.cpp

--- Product of two numbers entered by user.cpp
+++ Product of two numbers entered by user.cpp
@@ -3,10 +3,8 @@ using namespace std;
 int main()
 {
 	float a,b,r;
-	cout<<"Enter a number : ";
-	cin>>a;
-	cout<<"Enter another number : ";
-	cin>>b;
+	cout<<"Enter two numbers : ";
+	cin>>a>>b;
 	r = a*b;
 	cout<<"Product of "<< a <<" & "<< b <<" is : "<< r;
 	
